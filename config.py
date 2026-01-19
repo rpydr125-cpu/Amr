4c251db05fbf54b70e311ab78acbb9219ba9a1ff
rpydr125-cpu <rpydr125@gmail.com>
2026-01-19 20:54:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "12345"))
-    API_HASH = os.environ.get("API_HASH", "your_hash_here")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_token_here")
+    API_ID = int(os.environ.get("API_ID", "22447622"))
+    API_HASH = os.environ.get("API_HASH", "543b62d58d3e723e766ba57a984ab65d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8296583593:AAFdO5i9cj9noPqmeVZl9kbH4nEWWwmI42w")
     # Aapki Akamai API
     API_URL = "https://hindibhaskarbyamarnathapi.akamai.net.in/xstream?url="
   
